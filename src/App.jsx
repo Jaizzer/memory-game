1f30b3af44a5337c7f60e371bb3b2b44772153d7
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-23 15:46:48 +0800

    feat: Retrieve demon slayer API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { useEffect } from 'react';
+
 export default function App() {
+    // Retrieve demon slayer API
+    useEffect(() => {
+        fetch('https://demon-slayer-api.onrender.com/v1/').then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        }).then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error('Error: ', error);
+        });
+    }, []);
     return <></>;
 }
