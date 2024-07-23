f3ced189cf265277f438764a86b95e839faec452
Florenz Jaizzer Calderon <florenzjaizzer.calderon@gmail.com>
2024-07-23 16:00:20 +0800

    style: Format code

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ export default function App() {
 
     // Retrieve demon slayer API
     useEffect(() => {
-        fetch('https://demon-slayer-api.onrender.com/v1/').then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json()
-        }).then(data => {
-            setCharacters(data.map(character=> {return {...character, clickCount: 0}}));
-        }).catch(error => {
-            console.error('Error: ', error);
-        });
+        fetch('https://demon-slayer-api.onrender.com/v1/')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setCharacters(
+                    data.map((character) => {
+                        return { ...character, clickCount: 0 };
+                    })
+                );
+            })
+            .catch((error) => {
+                console.error('Error: ', error);
+            });
     }, []);
     return <></>;
 }
